be4643bfdc67fd14afd347d987b21822416dda83
Robert Banea <robertbanea113@gmail.com>
2021-12-14 21:19:37 +0100

    add pupil added + homepage navigation

--- addPupil.php
+++ addPupil.php
@@ -5,10 +5,64 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-        <link href="assets/css/pupilAddSort.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/index.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/addNsort.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        
+        <div class="centertext">
+            <?php
+                include 'php/menu.php';
+            ?>
+
+            <h1>
+                Add new pupil!
+            </h1>
+
+            <br>
+
+            <h2>
+                <form method="GET">
+                    <div class="formElement">
+                        <label id="inputNameLabel" for="inputName">
+                            Name
+                        </label>
+                        <br>
+                        <input type="text" id="inputName">
+                    </div>
+                    
+                    <div class="formElement">
+                        <label for="inputFavFood">
+                            Favourite food
+                        </label>
+                        <br>
+                        <input type="text" id="inputFavFood">
+                    </div>
+
+                    <br>
+
+                    <div class="formElement">
+                        <label for="inputFavActivity">
+                            Favourite activity
+                        </label>
+                        <br>
+                        <input type="text" id="inputFavActivity">
+                    </div>
+
+                    <div class="formElement">
+                        <label for="inputBestTrait">
+                            Best trait
+                        </label>
+                        <br>
+                        <input type="text" id="inputBestTrait">
+                    </div>
+                    <br><br>
+
+                    <button id="submitButton" type="submit">
+                        Add pupil!
+                    </button>
+                </form>
+            </h2>
+        </div>
     </body>
 </html>
 <?php
--- php/menu.php
+++ php/menu.php
@@ -5,11 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-        <link href="assets/css/pupilAddSort.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <?php
-            include 'php/menu.php';
-        ?>
+        <nav id="homeNav">
+            <a href="./index.html">Home</a>
+        </nav>
+        <br>
     </body>
-</html>
+    </html>
